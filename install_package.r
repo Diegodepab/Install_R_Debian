0e391a529ba1ebc9408388b8cc6598671b969a26
Diego De Pablo <91531665+Diegodepab@users.noreply.github.com>
2024-09-16 18:01:36 +0200

    Update install_package.r
    translate to english

--- install_package.r
+++ install_package.r
@@ -1,79 +1,78 @@
-# Para usuarios de Linux ####
+# For Linux users ####
 #
-# Antes de instalar estos paquetes, conviene instalar tres que son específicos de linux
-# y que, si no los tienes, no va a funcionar. Se instalan desde `bash` en **Ubuntu**, en los siguientes pasos:
+# Before installing these packages, it's recommended to install three specific to Linux
+# that, if missing, will prevent functionality. Install them via `bash` in **Ubuntu** using the following steps:
 #
-# Para mantener actualizados todos los paquetes en un linux basado en Debian, como Ubutuntu. 
-# Más información en <https://www.freecodecamp.org/news/sudo-apt-get-update-vs-upgrade-what-is-the-difference/>
+# To keep all packages up-to-date in a Debian-based Linux like Ubuntu. 
+# More information at <https://www.freecodecamp.org/news/sudo-apt-get-update-vs-upgrade-what-is-the-difference/>
 # `$ sudo apt-get update` 
 #
-# Para ver cuáles son los que se han actualizado, usa
+# To check which packages have been updated, use:
 # `$ sudo apt-get upgrade` 
 #
-# Para instalar el R más completo, incluso con librerías a compilar. 
-# Más info en <https://cran.r-project.org/bin/linux/ubuntu/fullREADME.html>
+# To install the full version of R, including libraries to compile.
+# More info at <https://cran.r-project.org/bin/linux/ubuntu/fullREADME.html>
 # `$ sudo apt-get install r-base r-base-dev` 
 #
-# Instalación de las dependencias principales
+# Installation of main dependencies
 # `$ sudo apt-get install libblas-dev liblapack-dev libgfortran-8-dev gfortran`
 # 
-# Instala estas otras librerías que podrían requerir algunos paquetes de R
+# Install these other libraries that some R packages may require:
 # `$ sudo apt-get install libatlas3-base libssl-dev libclang-14-dev libclang-dev libcurl4-openssl-dev`
 # `$ sudo apt-get install libzmq3-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev` 
 # `$ sudo apt-get install libtiff5-dev libjpeg-dev build-essential libfontconfig1-dev`
 # `$ sudo apt-get install libnlopt-dev libxml2-dev libpoppler-cpp-dev cmake`
 #
-# Optativamente, puedes instalar también estas otras librerías para java y otros lenguajes:
+# Optionally, you can install these other libraries for Java and other languages:
 # `$ sudo apt-get install -y r-cran-xml r-cran-rjava openjdk-7-* libgdal-dev libproj-dev libgsl-dev`
 # `$ sudo apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev`
 # `$ sudo apt-get install -y libx11-dev r-cran-rgl r-cran-rglpk r-cran-rsymphony r-cran-plyr`
 # `$ sudo apt-get install -y  r-cran-reshape  r-cran-reshape2 r-cran-rmysql`
 #
-# Si no se han conseguido instalar todas las dependencias, corrígelo con 
+# If some dependencies were not installed, fix it with:
 # `$ sudo apt --fix-broken install`
 
-# Si al instalar los paquetes de R posteriores obtienes un mensaje de error de tipo 
+# If you get an error message when installing R packages like 
 #    /usr/bin/ld: cannot find -lgfortran
-# habrás de asegurarte que esa librería está en el directorio /usr/bin/ con el siguiente comando de bash para crear un enlace simbólico en dicho directorio:
+# ensure that this library is in the /usr/bin/ directory by creating a symbolic link with this bash command:
 # `$ sudo ln -s /usr/lib/x86_64-linux-gnu/libgfortran.so.5 /usr/lib/libgfortran.so`
 #
-# If you want to change the location of non-base libraries installes, you can change globally the forder with
+# If you want to change the location of non-base libraries installed, you can globally change the folder with
 # 
 # `> .libPaths("path-to-the-new-folder")`
 #
-# or you can install only certain libraries in another folder using the `lib` parameter of `install.packages` as
+# or you can install certain libraries in another folder using the `lib` parameter of `install.packages` as
 # 
 # `> install.packages(new_libs, dependencies = TRUE, lib = "path-to-the-new-folder")`
 # 
 
 # //////////////////////////
-# Cómo generar un HTML ####
+# How to generate an HTML ####
 #
-# Con este script puedes generar un HTML de varias maneras:
-# 1) pulsando el botón de la libretita  denominado "Compile Report" de RStudio 
-# 2) Con el atajo de teclado MAY-CMD-K (mac) o MAY-CTRL-K (windows o linux)
-# 3) Con el comando rmarkdown::render("install-libraries_GISa-HAB.R"), siempre que ya tengas instalada la librería 'rmarkdown'
+# You can generate an HTML in different ways with this script:
+# 1) Press the "Compile Report" button in RStudio 
+# 2) Use the keyboard shortcut SHIFT-CMD-K (Mac) or SHIFT-CTRL-K (Windows or Linux)
+# 3) Use the command rmarkdown::render("install-libraries_GISa-HAB.R"), provided you have installed the 'rmarkdown' package
 #
-# Pero igual te da mensajes de error o se queda pillado, así que mejor no lo intentes por ahora.
+# However, you may encounter error messages or the process may freeze, so it's better not to try it for now.
 #
 
 
 # ///////////////////////////
-# Paquetes ya instalados ####
+# Installed packages ####
 # 
 # More details <https://www.r-bloggers.com/an-efficient-way-to-install-and-load-r-packages/>
 
 installed_libs <- rownames(installed.packages())
-cat("Tienes instaladas ", length(installed_libs), " paquetes de R en tu ordenador \n")
-cat("Las nuevas librerías se instalarán en el directorio \n")
+cat("You have ", length(installed_libs), " R packages installed on your computer \n")
+cat("New libraries will be installed in the directory \n")
 .libPaths()
 
 
 # ////////////////////////////////////
-# Instalación de paquetes de CRAN ####
+# CRAN packages installation ####
 # 
-# Creamos un vector con el nombre de los paquetes necesarios para el curso
-
+# We create a vector with the names of the packages needed for the course
 
 cat("\n*** Checking CRAN libraries that must be installed *** \n\n")
 
@@ -87,15 +86,15 @@ libs_HAB_CRAN <- c("R.utils", "digest", "rlang", "fastmap", "htmltools", "xfun",
               "fpc", "mclust", "amap", "ggfortify", "gptstudio", "randomForest", "LogicReg", "elasticnet", 
               "visNetwork", "rpart.plot", "GWalkR", "quarto")
 
-# Comparar las librerías instaladas con las librerías a instalar para descargar solo las que necesiten instalación
+# Compare installed libraries with those to be installed, downloading only the ones that need installation
 new_libs_CRAN <- libs_HAB_CRAN[!(libs_HAB_CRAN %in% installed_libs)]
 
 cat(length(new_libs_CRAN), " libraries to be installed from CRAN: \n", paste(new_libs_CRAN, collapse = ", "), "\n\n")
 
 
-# Instalación propiamente dicha de las librerías que haya en el vector `new_libs_CRAN`, 
-# siempre que este no esté vacío (señal de que ya están todas instaladas), 
-# que se mira con `length(new_libs_CRAN) > 0`.
+# Actual installation of the libraries in the `new_libs_CRAN` vector, 
+# provided it's not empty (indicating all are installed), 
+# which is checked with `length(new_libs_CRAN) > 0`.
 
 # set default repository 
 CRAN_REPOS <- getOption("repos") # use default repository
@@ -116,12 +115,12 @@ rm(libs_HAB_CRAN, new_libs_CRAN, CRAN_REPOS)
 
 
 # ////////////////////////////////////////////
-# Instalación de paquetes de Bioconductor ####
+# Bioconductor packages installation ####
 # 
-# Encontrarás más información al respecto en <https://www.bioconductor.org/install/>. 
-# Lo primero será comprobar qué paquetes son necesarios y no están todavía instalados 
-# (o sea, ausentes en el vector `installed_libs`). De paso, instalaremos el paquete 
-# `BiocManager` necesario para instalar estas librerías.
+# You'll find more information at <https://www.bioconductor.org/install/>. 
+# First, we will check which packages are necessary and not yet installed 
+# (absent in the `installed_libs` vector). Along the way, we'll install the 
+# `BiocManager` package needed to install these libraries.
 
 cat("\n*** Checking Bioconductor libraries that must be installed  ***\n\n")
 
@@ -131,7 +130,7 @@ if (!require("BiocManager", quietly = TRUE))
 # BiocManager::install(version = "3.16") # for R > 4.3
 BiocManager::install(ask = FALSE)
 
-# Si usar un R más antiguo, recibirás un mensaje de error del tipo
+# If using an older version of R, you'll receive an error message like:
 # 
 # Bioconductor version '3.18' is out-of-date; the current release version '3.19' is available with R version '4.4'; see
 # https://bioconductor.org/install
@@ -140,11 +139,11 @@ BiocManager::install(ask = FALSE)
 # Error: Bioconductor version '3.16' requires R version '4.2'; use `version = '3.18'` with R version 4.3; see
 # https://bioconductor.org/install
 # 
-# En tal caso, modifica el código para la versión que te recomiendan, que es la compatible con tu R. También puedes hacer una instalación básica con:
+# In that case, modify the code to the recommended version compatible with your R. You can also perform a basic installation with:
 # 
 # > BiocManager::install(ask = FALSE)
 
-# Nuevo vector con las librerías de Bioconductor que nos harán falta.
+# New vector with the required Bioconductor libraries.
 libs_HAB_BioC <- c("edgeR", 
                "limma", 
                "RColorBrewer",
@@ -153,24 +152,24 @@ libs_HAB_BioC <- c("edgeR",
                "pathview",
                "Biostrings")
 
-# Obtenemos la lista de las librerías que hay que instalar, como antes, siempre que haya algo que instalar
-nuevos_BioC <- libs_HAB_BioC[!(libs_HAB_BioC %in% installed_libs)]
+# Obtain the list of libraries to install, as before, if there's anything to install
+new_BioC <- libs_HAB_BioC[!(libs_HAB_BioC %in% installed_libs)]
 
-cat("Libraries to be installed from Bioconductor: \n", paste(nuevos_BioC, collapse = ", "), "\n\n")
+cat("Libraries to be installed from Bioconductor: \n", paste(new_BioC, collapse = ", "), "\n\n")
     
-# installation provided that 'nuevos_BioC' vector is not empty
-if (length(nuevos_BioC)) {
-    BiocManager::install(nuevos_BioC, ask = FALSE)
-    cat("INSTALLED ", length(nuevos_BioC), " BioConductor ", BiocManager::version(), " libraries for ", R.version.string, "\n")
-    cat(toString(nuevos_BioC)) 
+# installation provided that 'new_BioC' vector is not empty
+if (length(new_BioC)) {
+    BiocManager::install(new_BioC, ask = FALSE)
+    cat("INSTALLED ", length(new_BioC), " BioConductor ", BiocManager::version(), " libraries for ", R.version.string, "\n")
+    cat(toString(new_BioC)) 
 } else {
 	message(paste(sep="", "BioConductor ", BiocManager::version(), " update not required"))
 }
 
 # remove needless variables
-rm(installed_libs, libs_HAB_BioC, nuevos_BioC)
+rm(installed_libs, libs_HAB_BioC, new_BioC)
 
 # //////////////////////
-# Información final ####
+# Final information ####
 
-message("\n All required ibraries for ", R.version.string, " in ", R.version$platform, " were installed\n")
+message("\n All required libraries for ", R.version.string, " in ", R.version$platform, " were installed\n")
